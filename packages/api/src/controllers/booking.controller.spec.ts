0b6b89832c6ee6b4fbb5420564cf1abd5aea1bf2
Jorge Luis Sarmiento Herrera <yorchsarmiento@gmail.com>
2022-11-25 15:32:45 -0500

    Crud actions

--- packages/api/src/controllers/booking.controller.spec.ts
+++ packages/api/src/controllers/booking.controller.spec.ts
@@ -1,8 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BookingController } from './booking.controller';
 import { AppService } from '../services/app.service';
+import { BookingDto } from '../models/dtos/booking.dto';
 
-describe('AppController', () => {
+describe('BookingController', () => {
   let controller: BookingController;
 
   beforeEach(async () => {
@@ -14,9 +15,30 @@ describe('AppController', () => {
     controller = app.get<BookingController>(BookingController);
   });
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
+  describe('Booking Contoller - CRUD', () => {
+    it('should return Booking list', () => {
       expect(controller.getBookings(2)).toBeDefined();
+    });
+
+    it('should return Booking details', () => {
+        expect(controller.getBooking('0000-1111')).toBeDefined();
+    });
+
+    it('should create a new booking', () => {
+        const expectedResult = 'new booking created';
+        const newBooking = new BookingDto(null, '0000-1111', 'ABC123', '01-12-2022');
+        expect(controller.createBooking(newBooking)).toBe(expectedResult);
+    });
+
+    it('should modify a booking', () => {
+        const expectedResult = 'Modified this booking 2222-3333';
+        const editableBooking = new BookingDto('2222-3333', '0000-1111', 'ABC123', '01-12-2022');
+        expect(controller.modifyBooking('2222-3333', editableBooking)).toBe(expectedResult);
+    });
+
+    it('should delete a booking', () => {
+        const expectedResult = 'Deleted this booking 2222-3333';
+        expect(controller.deleteBooking('2222-3333')).toBe(expectedResult);
     });
   });
 });
--- packages/api/src/controllers/booking.controller.ts
+++ packages/api/src/controllers/booking.controller.ts
@@ -10,17 +10,38 @@ import {
     Param, 
     Query } from "@nestjs/common"; 
 import { Request, Response } from "express";
+import { identity } from "rxjs";
 import { BookingDto } from "../models/dtos/booking.dto";
 
-@Controller('api')
+@Controller('api/bookings')
 export class BookingController {
     constructor() {}
 
-    @Get('bookings')
+    @Get()
     getBookings(@Query('limit') limit: number): Array<BookingDto> {
         return new Array<BookingDto>(
             new BookingDto('000-1111-2222','333-4444-5555','RZO316','01-12-2022'),
             new BookingDto('001-1112-2223','334-4445-5556','ABC345','01-12-2022') 
         );
+    }
+
+    @Get(':id')
+    getBooking(@Param('id') id: string): BookingDto {
+        return new BookingDto('001-1112-2223','334-4445-5556','ABC345','01-12-2022') 
+    }
+
+    @Post()
+    createBooking(@Body() bookingDto: BookingDto) {
+        return `new booking created`;
+    }
+
+    @Put(':id')
+    modifyBooking(@Param('id') id: string, @Body() bookingDto: BookingDto) {
+        return `Modified this booking ${bookingDto.bookingId}`;
+    }
+    
+    @Delete(':id')
+    deleteBooking(@Param('id') id: string) {
+        return `Deleted this booking ${id}`
     }
 }
